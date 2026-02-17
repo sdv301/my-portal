f251f7f39a50d753c1d89a2cab0de73358ce98ba
Danil <you@example.com>
2026-02-17 10:19:37 +0900

    1

--- react-app/src/components/Map.jsx
+++ react-app/src/components/Map.jsx
@@ -100,7 +100,8 @@ const Map = ({ filters, onDistrictClick, setDistrictData }) => {
       maxZoom={12}
     >
       <TileLayer
-        url="/tiles/yakutia/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <SetBounds bounds={yakutiaBounds} />
 
